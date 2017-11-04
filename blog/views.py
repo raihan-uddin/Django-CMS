dfdd2076e5bf5faac20bb64f61b6b15854159872
Raihan <blackedition.me@gmail.com>
2017-11-04 20:28:04 +0600

    first django view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def list_of_post(request):
+    post = Post.objects.all()
+    tamplate = 'blog/post/list_of_post.html'
+    context = {'post': post}
+    return render(request, tamplate, context)
