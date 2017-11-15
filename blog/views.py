d299674c315a82dfff32862dd175b10f85560c48
Raihan Uddin <blackedition.me@gmail.com>
2017-11-15 11:37:16 +0600

    code reformated

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Post
 
+
 # Create your views here.
 def list_of_post(request):
     post = Post.objects.all()
     tamplate = 'blog/post/list_of_post.html'
+    ttt = 'b'
     context = {'post': post}
     return render(request, tamplate, context)
