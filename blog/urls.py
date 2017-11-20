6c0e62be34d9611be55fa8e5b866c12b397beac9
Raihan Uddin <blackedition.me@gmail.com>
2017-11-21 01:59:54 +0600

    setting views for blog categories

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.list_of_post, name='list_of_post'),
-    url(r'^(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail')
+    url(r'^(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail'),
+    url(r'category/(?P<category_slug>[-\w]+)/$', views.list_of_post_by_category, name='list_of_post_by_category'),
 ]
