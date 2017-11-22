8b7abc8d9ee1d69ca7673a8e0d5fabf2b55147e7
Raihan Uddin <blackedition.me@gmail.com>
2017-11-22 10:04:57 +0600

    comment form added adding comment form

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     url(r'^$', views.list_of_post, name='list_of_post'),
     url(r'^(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail'),
     url(r'category/(?P<category_slug>[-\w]+)/$', views.list_of_post_by_category, name='list_of_post_by_category'),
+    url(r'^(?P<slug>[-\w]+)/comment/$', views.add_comment, name='add_comment')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Category
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -7,7 +8,7 @@ def list_of_post_by_category(request, category_slug):
     categories = Category.objects.all()
     post = Post.objects.filter(status='published')
     if category_slug:
-        category = get_object_or_404(Category, slug = category_slug)
+        category = get_object_or_404(Category, slug=category_slug)
         post = post.filter(category=category)
     template = 'blog/category/list_of_post_category.html'
     context = {'categories': categories, 'post': post, 'category': category}
@@ -25,4 +26,20 @@ def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     template = 'blog/post/post_detail.html'
     context = {'post': post}
+    return render(request, template, context)
+
+
+def add_comment(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('blog:post_detail', slug=post.slug)
+    else:
+        form = CommentForm()
+    template= 'blog/post/add_comment.html'
+    context = {'form': form}
     return render(request, template, context)
